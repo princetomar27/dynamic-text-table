c3045c2badec61faa695a595ae3783cc0870a7fb
Prince Tomar <prince@farmsetu.co>
2025-02-13 00:18:36 +0530

    updated test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dynamic_text_table/main.dart';
+import '../example/example.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
